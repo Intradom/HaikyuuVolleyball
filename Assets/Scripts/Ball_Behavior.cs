d0ab136f49eba978dc7f25a708dbe7f7c86d0f49
Bo <zboyang@umich.edu>
2020-06-21 20:19:51 -0400

    Added in previous player controller which enables smoother movement and jumps

---
+++ Assets/Scripts/Ball_Behavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball_Behavior : MonoBehaviour
+{
+    [SerializeField]
+    private string tag_out = "";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == tag_out)
+        {
+            // Reset scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     [SerializeField]
+    private Player_Control script_player_control;
+    [SerializeField]
     private float player_speed = 0;
-
-    private Rigidbody2D ref_rbody;
-
-    void Awake()
-    {
-        ref_rbody = GetComponent<Rigidbody2D>();
-    }
 
     void FixedUpdate()
     {
-        float vel_x = Time.fixedDeltaTime * player_speed * Input.GetAxis("Horizontal");
+        float move_x = Time.fixedDeltaTime * player_speed * Input.GetAxis("Horizontal");
 
-        ref_rbody.AddForce(new Vector2(vel_x, 0));
+        script_player_control.Move(move_x, false, Input.GetButton("Jump"));
     }
 }
